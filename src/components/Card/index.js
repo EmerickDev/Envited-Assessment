3c058554e08940ac486f449271b35ffb98af98ff
Guilherme Emerick <gulhermeemerick@gmail.com>
2022-10-02 18:15:03 +0800

    idk why but te component past was missing, sorry!

---
+++ src/components/Card/index.js
@@ -0,0 +1,11 @@
+import myCard from "../../img/image.svg";
+
+const Card = () => {
+  return (
+    <>
+      <img src={myCard} alt="Card"></img>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/components/Content/index.js
@@ -0,0 +1,24 @@
+const Content = () => {
+  return (
+    <>
+      <div className="content">
+        <p className="text">Imagine if</p>
+        <p className="snapchat">Snapchat</p>
+        <p className="text">had Events</p>
+
+        <p className="sub-text">
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+
+        <div className="btn-btn">
+          <button type="submit" form="form1" value="Submit">
+            {" "}
+            <p className="btn-text">🎉 Create my event</p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Content;
